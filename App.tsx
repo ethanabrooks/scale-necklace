ba40633258a6d2d700c6dda3b1ffc927ae21352f
Ethan Brooks <ethanabrooks@gmail.com>
2020-10-27 13:30:29 -0400

    got scale working using Timer as model

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Text, TouchableOpacity, View } from "react-native";
 import "./scales";
 import { scales } from "./scales";
-import { now, Synth } from "tone";
+import { Synth } from "tone";
 import { styles } from "./styles";
-import { Seconds } from "tone/build/esm/core/type/Units";
 import { Note, notes, NUM_NOTES } from "./notes";
 
 type Scale = number[];
@@ -17,22 +16,146 @@ function mod(a: number, b: number): number {
   return ((a % b) + b) % b;
 }
 
-type State = { loaded: false } | { loaded: true; synth: Synth; now: Seconds };
+type State =
+  | { loaded: false }
+  | {
+      loaded: true;
+      synth: Synth;
+      notesToPlay: Note[];
+      octave: number;
+      seconds: number;
+    };
 
-export default function App(): JSX.Element {
-  const [scale, setScale] = React.useState<Scale>(scales[0]);
-  const [root, setRoot] = React.useState<number>(0);
+function Player(props: { notesInScale: Note[] }): JSX.Element {
   const [state, setState] = useState<State>({ loaded: false });
-  const [notesToPlay, setNotesToPlay] = useState<number[]>([]);
+  useEffect(() => {
+    console.log("here");
+    // start().then(() => {
+    const synth = new Synth().toDestination();
+    setState({
+      loaded: true,
+      synth: synth,
+      notesToPlay: [],
+      octave: 3,
+      seconds: 0,
+    });
+    // }
+    // );
+  }, [setState]);
 
   useEffect(() => {
+    if (state.loaded && state.notesToPlay.length > 0) {
+      const [head, ...tail]: Note[] = state.notesToPlay;
+      // state.synth.triggerAttack(`${head.sharp}${state.octave}`);
+      // console.log(`${head.sharp}${state.octave}`);
+      console.log(tail);
+      setInterval(() => {
+        // console.log("release 1");
+        setState({ ...state, notesToPlay: tail });
+      }, 1000);
+      return () => {
+        // console.log("release 2");
+        // state.synth.triggerRelease();
+      };
+    }
+    return () => {};
+  }, [setState]);
+  if (!state.loaded) {
+    return <Text>loading...</Text>;
+  }
+
+  return (
+    <View style={styles.button}>
+      <Button
+        title={"Play"}
+        onPress={() => {
+          setState({ ...state, notesToPlay: props.notesInScale });
+        }}
+      />
+    </View>
+  );
+}
+
+function Timer(props: { notesInScale: Note[] }) {
+  const [isActive, setIsActive] = useState(false);
+  const [state, setState] = useState<State>({ loaded: false });
+
+  useEffect(() => {
+    // start().then(() => {
     const synth = new Synth().toDestination();
-    setState({ loaded: true, synth: synth, now: now() });
-  }, []);
+    setState({
+      loaded: true,
+      synth: synth,
+      notesToPlay: [],
+      octave: 3,
+      seconds: 0,
+    });
+    // });
+  }, [setState]);
+
+  useEffect(() => {
+    let interval: null | number = null;
+    if (state.loaded) {
+      if (isActive) {
+        if (state.notesToPlay.length > 0) {
+          const [head, ...tail]: Note[] = state.notesToPlay;
+          state.synth.triggerAttack(`${head.sharp}${state.octave}`);
+          interval = setInterval(() => {
+            setState({
+              ...state,
+              seconds: state.seconds + 1,
+              notesToPlay: tail,
+            });
+          }, 1000);
+        }
+      } else if (!isActive && state.seconds !== 0) {
+        clearInterval((interval as unknown) as number);
+      }
+    }
+    return () => {
+      if (state.loaded) state.synth.triggerRelease();
+      clearInterval((interval as unknown) as number);
+    };
+  }, [isActive, state]);
 
   if (!state.loaded) {
     return <Text>loading...</Text>;
   }
+
+  function toggle() {
+    if (state.loaded) setState({ ...state, notesToPlay: props.notesInScale });
+  }
+
+  function reset() {
+    if (state.loaded) {
+      setState({ ...state, seconds: state.seconds + 1 });
+    }
+    setIsActive(false);
+  }
+
+  return (
+    <div className="app">
+      <div className="time">{state.seconds}s</div>
+      <div className="row">
+        <button
+          className={`button button-primary button-primary-${
+            isActive ? "active" : "inactive"
+          }`}
+          onClick={toggle}
+        >
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function App(): JSX.Element {
+  const [scale, setScale] = React.useState<Scale>(scales[0]);
+  const [root, setRoot] = React.useState<number>(0);
 
   const rootButton: JSX.Element = (
     <Button
@@ -47,23 +170,7 @@ export default function App(): JSX.Element {
     },
     [root]
   );
-  const Now = now();
-
-  const playButton: JSX.Element = (
-    <Button
-      title={"Play"}
-      onPress={() => {
-        const synth = new Synth().toDestination();
-        scaleIndices.forEach((index: number, i: number) => {
-          const note = notes[index % NUM_NOTES];
-          const octave = index < NUM_NOTES ? 3 : 4;
-          synth.triggerAttack(`${note.sharp}${octave}`, Now + i);
-          synth.triggerRelease(Now + i + 0.5);
-        });
-      }}
-    />
-  );
-
+  const notesInScale = scaleIndices.map((i) => notes[i % NUM_NOTES]);
   const width = 250;
   const necklace = (
     <View
@@ -113,7 +220,9 @@ export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
       <View style={styles.button}>{rootButton}</View>
-      <View style={styles.button}>{playButton}</View>
+      <View style={styles.button}>
+        <Timer notesInScale={notesInScale} />
+      </View>
       <View style={styles.necklace}>{necklace}</View>
     </View>
   );
